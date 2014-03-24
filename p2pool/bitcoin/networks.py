d1b4139f5a500c56327f637b547de0c56c89f0bb
OmarxGx <omar.galal.16@gmail.com>
2014-03-24 12:56:16 -0400

    BlitzCoin Added
    Must fill in Prefix and ports

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -826,6 +826,27 @@ germanycoin=math.Object(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=1e8,
       ),
+blitzcoin=math.Object(
+        P2P_PREFIX='FILL'.decode('hex'),
+        P2P_PORT=FILL,
+        ADDRESS_VERSION=FILL,
+        RPC_PORT=Fill,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'blitzcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 10000*100000000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s
+        SYMBOL='BLTZ',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'teslax3') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/teslax3/') if platform.system() == 'Darwin' else os.path.expanduser('~/.teslax3'), 'teslax3.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='https://altexplorer.net/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='https://altexplorer.net/address/',
+        TX_EXPLORER_URL_PREFIX='https://altexplorer.net/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=1e8,
+      ),
 
 
 )
